56519a380f3a0f73a13da3f7338d4885df368f72
Nicolas Le Thierry d'Ennequin <nlte@magic.fr>
2021-12-01 18:05:25 +0100

    New model based (see reference inline)

--- src/App.svelte
+++ src/App.svelte
@@ -4,16 +4,20 @@
 
 <svelte:head><title>Menu</title></svelte:head>
 
-<main>
-  <Menu>
-    <ul>
-      <li><a href="/">Item 1</a></li>
-      <li><a href="/">Item 2</a></li>
-      <li><a href="/">Item 3</a></li>
-      <li><a href="/">Item 4</a></li>
-    </ul>
-  </Menu>
+<header>
+  <nav>
+    <Menu>
+      <ul>
+        <li><a href="/">Item 1</a></li>
+        <li><a href="/">Item 2</a></li>
+        <li><a href="/">Item 3</a></li>
+        <li><a href="/">Item 4</a></li>
+      </ul>
+    </Menu>
+  </nav>
+</header>
 
+<main>
   <p>Lorem ipsum.</p>
 </main>
 
@@ -21,18 +25,18 @@
   ul {
     list-style-type: none;
     padding: 0;
-    display: flex;
-    flex-direction: row;
-  }
-
-  li {
-    flex: 0 0 auto;
-    outline: solid 1px red;
   }
   a {
-    display: block;
-    padding: 2px 4px;
     text-decoration: none;
     color: inherit;
+  }
+
+  header {
+    border-bottom: 1px solid #e2e8f0;
+  }
+
+  nav {
+    display: flex;
+    padding: 1rem 0;
   }
 </style>
--- src/components/Menu.svelte
+++ src/components/Menu.svelte
@@ -8,72 +8,25 @@
   $: console.log(isOpen);
 </script>
 
-<div class="menu-bar">
-  <div class="menu-wrapper" class:open={isOpen}>
-    <div on:click={toggle} class="btn-menu">☰</div>
-    <div class="menu-container">
-      <slot />
-    </div>
-  </div>
-</div>
+<!-- Based on: https://dev.to/devggaurav/let-s-build-a-responsive-navbar-and-hamburger-menu-using-html-css-and-javascript-4gci -->
+
+<div on:click={toggle} class="btn" role="button">☰</div>
+<slot />
 
 <style>
-  .menu-bar {
-    outline: solid 1px orange;
-  }
-
-  .menu-wrapper {
-    position: relative;
-    display: flex;
-    flex-direction: row;
-    padding: 4px;
-  }
-
-  .btn-menu {
+  .btn {
     display: none;
-    flex: 0 0 auto;
     cursor: pointer;
   }
-  .menu-container {
-    flex: 0 1 auto;
+
+  :global(ul) {
+    display: flex;
+    flex-direction: row;
+    outline: solid 1px blue;
   }
 
-  @media (max-width: 600px) {
-    .btn-menu {
-      display: inline;
-      outline: solid 1px blue;
-    }
-
-    .menu-wrapper {
-      position: relative;
-      flex-direction: column;
-    }
-
-    .menu-wrapper.open {
-      position: fixed;
-      top: 0;
-      bottom: 0;
-      left: 0;
-      right: 25%;
-    }
-
-    .menu-container {
-      display: none;
-    }
-
-    .menu-wrapper.open .menu-container {
-      position: fixed;
-      top: 0;
-      bottom: 0;
-      left: 0;
-      right: 25%;
-      background-color: #9cf;
-      /* min-width: 50%;
-      max-width: 75%; */
-    }
-
-    .menu-container :global(ul) {
-      flex-direction: column;
-    }
+  :global(ul > li) {
+    margin-right: 4px;
+    outline: solid 1px red;
   }
 </style>
